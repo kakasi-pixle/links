7f219402c7d12effae40578723c95f40402f771c
kakasi-pixle <yhyakakasi@gmail.com>
2026-03-14 01:37:23 +0200

    Delete pages directory

--- pages/verify.js
+++
@@ -1,8 +0,0 @@
-export default function Handler(req, res) {
-  // محتوى التحقق اللي الشبكة اعطتهولك
-  const content = "2a742c3848bdaa03616e";
-
-  // خلي Response يكون نص فقط
-  res.setHeader("Content-Type", "text/plain");
-  res.status(200).send(content);
-}
