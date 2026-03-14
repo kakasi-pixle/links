7f981cfc9621799c987e06c0c84496dba671db7d
kakasi-pixle <yhyakakasi@gmail.com>
2026-03-14 03:57:40 +0200

    Add basic HTML structure to index.js

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-<meta name="f347bac45efc9a4e8aec2a742c3848bdaa03616e" content="f347bac45efc9a4e8aec2a742c3848bdaa03616e" />
+<!DOCTYPE html>
+<html>
+<head>
+<title>Ads Page</title>
+</head>
+
+<body>
+
+<h1>مرحبا</h1>
+
+<script>
+atOptions = {
+'key' : '1b6d063030f9a97f69978bbeabea752e',
+'format' : 'iframe',
+'height' : 300,
+'width' : 160,
+'params' : {}
+};
+</script>
+
+<script src="//www.highperformanceformat.com/1b6d063030f9a97f69978bbeabea752e/invoke.js"></script>
+
+</body>
+</html>
